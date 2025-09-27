57c33fb3d2f278c1b3612222b3ef01cc90f29b39
Keilikvist <91397999+Keilikvist@users.noreply.github.com>
2025-09-27 11:06:10 -0500

    Added AI AP integration and gave AI specific instructions for how to present results to the user

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
-function generateDestination(event) {
-  event.preventDefault();
-
-  new Typewriter("#generated-destination", {
-    strings: "Result will go here",
+function displayDestination(response) {
+  new Typewriter("#destination", {
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: "",
   });
+}
+
+function generateDestination(event) {
+  event.preventDefault();
+
+  let instructionsInput = document.querySelector("#user-instructions");
+  let apiKey = "fb994d97ce3o8893t14c0f33a0f08b79";
+  let prompt = `User instructions: Generate a list of four popular travel destinations found in ${instructionsInput.value}`;
+  let context =
+    "You are a well-travelled expert who is familiar with popular travel destinations within all European countries. Your mission is to generate a bulleted list of 4 popular destinations by following the user instructions. Please place a <br /> element between each bulleted item. Please sign 'SheCodes AI' inside a <strong> element at the bottom of the list.";
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  console.log("Generating list");
+  console.log(`Prompt: ${prompt}`);
+  console.log(`Context: ${context}`);
+
+  axios.get(apiUrl).then(displayDestination);
 }
 
 let travelFormElement = document.querySelector("#destination-generator");
