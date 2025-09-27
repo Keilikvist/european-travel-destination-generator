982de44a68b5c206a6c0034a2e4ee5cdadfb4562
Keilikvist <91397999+Keilikvist@users.noreply.github.com>
2025-09-27 10:13:58 -0500

    Added basic form handling and added Typewriter plugin

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
+function generateDestination(event) {
+  event.preventDefault();
 
+  new Typewriter("#generated-destination", {
+    strings: "Result will go here",
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+}
+
+let travelFormElement = document.querySelector("#destination-generator");
+travelFormElement.addEventListener("submit", generateDestination);
